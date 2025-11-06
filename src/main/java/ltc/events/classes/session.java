35e7f8d59097d863693adbd3af415717f0894ade
Fevereiro2 <a2124a15@estel.edu.pt>
2025-11-06 04:15:37 +0000

    ☑️ Classe seasion create

--- src/main/java/ltc/events/classes/session.java
+++ src/main/java/ltc/events/classes/session.java
@@ -1,4 +1,39 @@
 package ltc.events.classes;
 
+import java.sql.Timestamp;
+
 public class session {
+    private final int session_id;
+    private final String name;
+    private final String description;
+    private final String local;
+    private final String state;
+    private final String image;
+    private final Timestamp startdate;
+    private final Timestamp finaldate;
+
+    public session(int sessionId, String name, String description, String local, String state, String image, Timestamp startdate, Timestamp finaldate) {
+        this.session_id = sessionId;
+        this.name = name;
+        this.description = description;
+        this.local = local;
+        this.state = state;
+        this.image = image;
+        this.startdate = startdate;
+        this.finaldate = finaldate;
+    }
+
+    public int getId() { return session_id; }
+    public String getName() { return name; }
+    public String getDescription() { return description; }
+    public String getLocal() { return local; }
+    public String getState() { return state; }
+    public String getImage() { return image; }
+    public Timestamp getStartdate() { return startdate; }
+    public Timestamp getFinaldate() { return finaldate; }
+
+    @Override
+    public String toString() {
+        return name + " (" + local + ")";
+    }
 }
