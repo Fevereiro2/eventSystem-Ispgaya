366cb2c99aad90b34ef6836e3c44b1f0d2791332
Fevereiro2 <a2124a15@estel.edu.pt>
2025-11-06 15:01:25 +0000

    ☑️Rename file ☑️Modify the method timestamp name

--- src/main/java/ltc/events/classes/Session.java
+++ src/main/java/ltc/events/classes/Session.java
@@ -2,26 +2,28 @@ package ltc.events.classes;
 
 import java.sql.Timestamp;
 
-public class session {
+public class Session {
     private final int session_id;
     private final String name;
     private final String description;
     private final String local;
     private final String state;
     private final String image;
-    private final Timestamp startdate;
-    private final Timestamp finaldate;
+    private final Timestamp initialDate;
+    private final Timestamp finishDate;
 
-    public session(int sessionId, String name, String description, String local, String state, String image, Timestamp startdate, Timestamp finaldate) {
+    public Session(int sessionId, String name, String description, String local, String state, String image, Timestamp initialDate, Timestamp finishDate) {
         this.session_id = sessionId;
         this.name = name;
         this.description = description;
         this.local = local;
         this.state = state;
         this.image = image;
-        this.startdate = startdate;
-        this.finaldate = finaldate;
+        this.initialDate = initialDate;
+        this.finishDate = finishDate;
     }
+
+
 
     public int getId() { return session_id; }
     public String getName() { return name; }
@@ -29,8 +31,8 @@ public class session {
     public String getLocal() { return local; }
     public String getState() { return state; }
     public String getImage() { return image; }
-    public Timestamp getStartdate() { return startdate; }
-    public Timestamp getFinaldate() { return finaldate; }
+    public Timestamp getStartdate() { return initialDate; }
+    public Timestamp getFinaldate() { return finishDate; }
 
     @Override
     public String toString() {
