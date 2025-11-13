719236ca93ca84f3b19e784acfd1ddc6961bc099
fevereiro2 <a2124a15@estel.edu.pt>
2025-11-13 17:14:27 +0000

    ✅ make a class category

--- src/main/java/ltc/events/classes/category.java
+++ src/main/java/ltc/events/classes/category.java
@@ -1,6 +1,18 @@
 package ltc.events.classes;
 
 public class category {
-    public int id;
-    public String name;
+    private final int id;
+    public final String name;
+
+    private category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    public int getId() { return id; }
+    public String getName() { return name; }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
