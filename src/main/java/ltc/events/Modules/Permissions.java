e822aa4f782ac6e05af25ca9f66678b4288238ec
fevereiro2 <a2124a15@estel.edu.pt>
2025-11-14 16:55:51 +0000

    ✅ Create a class where control permissions by Participants

---
+++ src/main/java/ltc/events/Modules/Permissions.java
@@ -0,0 +1,25 @@
+package ltc.events.Modules;
+
+import ltc.events.classes.Participant;
+import ltc.events.classes.hashs.SessionEntry;
+
+public class Permissions {
+    public static boolean isModerador(){
+        Participant u = SessionEntry.getUser();
+        return u != null &&
+                u.getType() != null &&
+                u.getType().getName().equalsIgnoreCase("moderator");
+    }
+    public static boolean isParticipant(){
+        Participant u = SessionEntry.getUser();
+        return u != null &&
+                u.getType() != null &&
+                u.getType().getName().equalsIgnoreCase("participant");
+    }
+    public static boolean isAdmin(){
+        Participant u = SessionEntry.getUser();
+        return u != null &&
+                u.getType() != null &&
+                u.getType().getName().equalsIgnoreCase("admin");
+    }
+}
