1d56d9341e0d6ff15439efae03447c91495743a6
fevereiro2 <a2124a15@estel.edu.pt>
2025-11-15 04:30:12 +0000

    ✅ Implementar o construtor automatico vindo da db

--- src/main/java/ltc/events/classes/Types.java
+++ src/main/java/ltc/events/classes/Types.java
@@ -1,13 +1,26 @@
 package ltc.events.classes;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Types {
     private final int id;
     private final String name;
 
+    // Construtor normal
     public Types(int id, String name) {
         this.id = id;
         this.name = name;
     }
+
+    // 🔥 Construtor automático vindo da BD
+    public Types(ResultSet rs) throws SQLException {
+        this(
+                rs.getInt("types_id"),
+                rs.getString("name")
+        );
+    }
+
     public int getId() { return id; }
     public String getName() { return name; }
 
@@ -16,5 +29,3 @@ public class Types {
         return name;
     }
 }
-
-
