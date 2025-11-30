d8d775d09b17e5643d66df9f4aa660dfe6203b0c
Pedro Fevereiro <103332709+Fevereiro2@users.noreply.github.com>
2025-11-30 18:13:33 +0000

    Comentar linhas
    Completamente fix , já com comentarios

--- src/main/java/ltc/events/Modules/db.java
+++ src/main/java/ltc/events/Modules/db.java
@@ -3,23 +3,16 @@ package ltc.events.Modules;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-// import io.github.cdimascio.dotenv.Dotenv; // Já não é necessário se não usar variáveis
-
 public class db {
-
-    // Se estiver a usar o driver SQLite, a URL tem de começar com jdbc:sqlite:
-    private static final String URL = "jdbc:sqlite:eventos.db";
-    // O ficheiro eventos.db será criado/acedido na pasta raiz do projeto.
-
-    public static Connection connect() {
-        try {
-            // O DriverManager reconhece o driver SQLite através do prefixo da URL.
-            Connection conn = DriverManager.getConnection(URL);
-            System.out.println("✔ Ligação aberta ao SQLite!");
-            return conn;
-        } catch (SQLException e) {
-            System.err.println("❌ Erro ao ligar ao SQLite: " + e.getMessage());
-            return null;
+    private static final String URL = "jdbc:sqlite:eventos.db"; //String para a conexão entre o ficheiro eventos.db onde contem toda a info do programa
+    public static Connection connect() { //função para conectar a base de dados
+        try { //tenta executar senão salta para o catch
+            Connection conn = DriverManager.getConnection(URL);//Tenta estabelecer a conexão com a base de dados. Assume-se que 'URL' é uma constante  que contém o caminho/endereço do banco de dados SQLite.
+            System.out.println("✔ Ligação aberta ao SQLite!");// Se a linha acima for bem-sucedida, esta linha será executada.
+            return conn; //Retorna o objeto 'conn' para a instancia que foi declarada
+        } catch (SQLException e) { //senão conseguir executar, inicia o bloco de captura de erros e mostra o erro especifico aonde esta a dar conflito
+            System.err.println("❌ Erro ao ligar ao SQLite: " + e.getMessage()); //Imprime a mensagem de erro
+            return null; //Retorna o objeto como null , indicando que não foi possivel estabelecer a ligação
         }
     }
 }
