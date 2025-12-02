bf29e94d09d23401a39a6d897d8fc2d320762080
Pedro Fevereiro <103332709+Fevereiro2@users.noreply.github.com>
2025-12-02 04:54:23 +0000

    Add new fields to Participant class
    Added password, gender, nif, birthdate, and photo fields to the Participant class. Updated constructors and getter methods to support these new attributes for more comprehensive participant data handling.

--- src/main/java/ltc/events/classes/Participant.java
+++ src/main/java/ltc/events/classes/Participant.java
@@ -1,5 +1,6 @@
 package ltc.events.classes;
 
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -11,15 +12,25 @@ public class Participant {
     private final String email;
     private final String phone;
     private final Types type;
+    private final String password;
+    private final boolean gender;
+    private final String nif;
+    private final Date birthdate;
+    private final String photo;
 
 
 
-    public Participant(String id, String name, String email, String phone, Types typesid) {
+    public Participant(String id, String name, String email, String phone, Types typesid, String password, boolean gender, String nif, Date birthdate, String photo) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.phone = phone;
         this.type = typesid;
+        this.password = password;
+        this.gender = gender;
+        this.nif = nif;
+        this.birthdate = birthdate;
+        this.photo = photo;
     }
 
     public Participant(ResultSet rs) throws SQLException {
@@ -31,7 +42,12 @@ public class Participant {
                 new Types(
                         rs.getInt("types_id"),
                         rs.getString("types_name")
-                )
+                ),
+                rs.getString("password"),
+                rs.getBoolean("gender"),
+                rs.getString("nif"),
+                rs.getDate("birthdate"),
+                rs.getString("photo")
         );
     }
 
@@ -43,6 +59,11 @@ public class Participant {
     public String getEmail() { return email; }
     public String getPhone() { return phone; }
     public Types getType() { return type; }
+    public String getPassword() { return password; }
+    public boolean isGender() { return gender; }
+    public String getNif() { return nif; }
+    public Date getBirthdate() { return birthdate; }
+    public String getPhoto() { return photo; }
     @Override
     public String toString() {
         return name;
