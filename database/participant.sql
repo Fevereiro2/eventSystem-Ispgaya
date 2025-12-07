e675d3c9ec8a95396d9814546eefa8ce19e0c2d7
Pedro Fevereiro <103332709+Fevereiro2@users.noreply.github.com>
2025-12-07 06:28:54 +0000

    Remove password field from participant inserts
    Updated the participant.sql script to exclude the password field from INSERT statements. This change may reflect a schema update or a decision to handle passwords differently.

--- database/participant.sql
+++ database/participant.sql
@@ -1,3 +1,3 @@
-INSERT INTO Participant (participant_id, name, phone, email, types_id, password) VALUES (1, 'Pedro Fevereiro', '913635135', 'pedro@fevereiro.pt', 3, '$2a$12$x0YlTANaJGiNMPmmPkMrKuZ7rT8/pu28roVhGpmO6JjR5Vkx4yNXS');
-INSERT INTO Participant (participant_id, name, phone, email, types_id, password) VALUES (2, 'Pedro ', '931456178', 'pedro@me.pt', 2, '$2a$12$2kPGYwqqpBJOOaAJAiUAOuHajamtjYR2HK6bqesLDN6tKTPivP4f.');
-INSERT INTO Participant (participant_id, name, phone, email, types_id, password) VALUES (3, 'Teste', '913635135', 'pedro@gmail.com', 1, '123');
+INSERT INTO Participant (participant_id, name, phone, email, types_id) VALUES (1, 'Pedro Fevereiro', '913635135', 'pedro@fevereiro.pt', 3);
+INSERT INTO Participant (participant_id, name, phone, email, types_id) VALUES (2, 'Pedro ', '931456178', 'pedro@me.pt', 2);
+INSERT INTO Participant (participant_id, name, phone, email, types_id) VALUES (3, 'Teste', '913635135', 'pedro@gmail.com', 1);
