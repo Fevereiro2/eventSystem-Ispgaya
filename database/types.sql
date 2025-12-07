f63b3591e5a096c7cd13f55e7d2aba694bfeac0e
Pedro Fevereiro <103332709+Fevereiro2@users.noreply.github.com>
2025-12-07 06:13:17 +0000

    Update types.sql

--- database/types.sql
+++ database/types.sql
@@ -1,3 +1,4 @@
 INSERT INTO Types (types_id, name) VALUES (1, 'Moderador');
 INSERT INTO Types (types_id, name) VALUES (2, 'Participante');
 INSERT INTO Types (types_id, name) VALUES (3, 'Admin');
+INSERT INTO Types (types_id, name) VALUES (4, 'Palestrante');
